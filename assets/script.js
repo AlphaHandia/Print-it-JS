0087d0f7d6f492bebeabf7d6abf2f236d23afa0b
Alix Hennion <hennion.pierre.alix@hotmail.fr>
2023-05-26 12:19:52 +0200

    Ajout des commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Définition des slides avec les images et les lignes de tag
 const slides = [
   {
     image: "./assets/images/slideshow/slide1.jpg",
@@ -17,14 +18,18 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
+
+// Sélection des éléments du DOM
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const dotsContainer = document.querySelector(".dots");
 const imageContainer = document.querySelector(".banner-img");
 
+// Index du slide actuel
 let currentSlideIndex = 0;
 
-function createDotPoint(slide, index) {
+// Fonction pour créer un point de repère (dot) pour chaque slide
+function createDotPoint(index) {
   const newDot = document.createElement("li");
   newDot.classList.add("dot");
   dotsContainer.appendChild(newDot);
@@ -34,6 +39,7 @@ function createDotPoint(slide, index) {
   });
 }
 
+// Fonction pour afficher le slide correspondant à l'index donné
 function goToSlide(index) {
   if (index < 0 || index >= slides.length) {
     return;
@@ -41,6 +47,7 @@ function goToSlide(index) {
 
   currentSlideIndex = index;
 
+  // Met à jour la classe CSS des dots pour indiquer le slide actuel
   const dots = dotsContainer.querySelectorAll(".dot");
   dots.forEach((dot, i) => {
     if (i === currentSlideIndex) {
@@ -50,14 +57,16 @@ function goToSlide(index) {
     }
   });
 
+  // Récupère le slide à afficher
   const slideToShow = slides[currentSlideIndex];
 
-  // Afficher l'image associée dans le conteneur
-  imageContainer.style.backgroundImage = `url('${slideToShow.image}')`;
+  // Met à jour l'image et la ligne de tag dans le DOM
+  imageContainer.src = `${slideToShow.image}`;
   const tagLineContainer = document.querySelector("#banner p");
   tagLineContainer.innerHTML = slideToShow.tagLine;
 }
 
+// Gestionnaire d'événement pour le bouton de la flèche gauche
 arrowLeft.addEventListener("click", () => {
   const prevSlideIndex = currentSlideIndex - 1;
   if (prevSlideIndex < 0) {
@@ -67,6 +76,7 @@ arrowLeft.addEventListener("click", () => {
   }
 });
 
+// Gestionnaire d'événement pour le bouton de la flèche droite
 arrowRight.addEventListener("click", () => {
   const nextSlideIndex = currentSlideIndex + 1;
   if (nextSlideIndex >= slides.length) {
@@ -76,9 +86,10 @@ arrowRight.addEventListener("click", () => {
   }
 });
 
+// Crée un point de repère (dot) pour chaque slide
 slides.forEach((slide, index) => {
-  createDotPoint(slide, index);
+  createDotPoint(index);
 });
 
-// Afficher la première diapositive au chargement de la page
+// Affiche le premier slide au chargement de la page
 goToSlide(currentSlideIndex);
